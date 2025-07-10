02c740b9a709de557cdd31dfa0987188ab6cad8f
bagabum640 <101508781+bagabum640@users.noreply.github.com>
2025-07-10 16:15:23 +0300

    new

--- Assets/Scripts/Base/ResourceContainer.cs
+++ Assets/Scripts/Base/ResourceContainer.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class ResourceContainer : MonoBehaviour
 {
-    [SerializeField] private List<Resource> _resources;
+    [SerializeField] private List<Resource> _allResources;
     [SerializeField] private List<Resource> _reservedResources;
 
     public void AddResource(Resource resource)
     {
-        if (_resources.Contains(resource) == false)
-            _resources.Add(resource);
+        if (_allResources.Contains(resource) == false)
+            _allResources.Add(resource);
     }
 
     public void Remove(Resource resource)
     {
-        _resources.Remove(resource);
+        _allResources.Remove(resource);
         _reservedResources.Remove(resource);
     }
 
     public Resource FindResource(Vector3 fromPosition)
     {
-        return _resources
+        return _allResources
             .Where(resource => _reservedResources.Contains(resource) == false)
             .OrderBy(resource => (resource.transform.position - fromPosition).sqrMagnitude)
             .FirstOrDefault();
@@ -29,11 +29,12 @@ public class ResourceContainer : MonoBehaviour
 
     public bool TryReserve(Resource resource)
     {
-        if (_resources.Contains(resource) && _reservedResources.Contains(resource) == false)
+        if (_allResources.Contains(resource) && _reservedResources.Contains(resource) == false)
         {
             _reservedResources.Add(resource);
             return true;
         }
+
         return false;
     }
 }
